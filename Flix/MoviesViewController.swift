d00251b658a612eae9eaf371bf91ce8df12cd27f
jasonbodzy <jasonbodzy@gmail.com>
2022-01-22 13:27:50 -0500

    trial

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -1,8 +1,43 @@
 //
-//  MoviesViewController.swift
+//  ViewController.swift
 //  Flix
 //
 //  Created by Jason Bodzy on 1/21/22.
 //
 
-import Foundation
+import UIKit
+
+class MoviesViewController: UIViewController {
+    
+    var movies = [[String:Any]]() //creation of an array of dictionaries
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello")
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                 
+                 self.movies = dataDictionary["results"] as! [[String:Any]]
+                 
+                    print(dataDictionary)
+                    // TODO: Get the array of movies
+                    // TODO: Store the movies in a property to use elsewhere
+                    // TODO: Reload your table view data
+
+             }
+        }
+        task.resume()
+    }
+
+
+}
+
